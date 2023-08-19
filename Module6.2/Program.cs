45b03c54608019ac1e9e58c082c6f456cb9415df
Ekaterina Volkova <koweevaea@gmail.com>
2023-08-19 19:40:26 +0500

    Модуль 6.2 Классы и структуры

---
+++ Module6.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Module6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Pen
+    {
+        public string color;
+        public int cost;
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+        public Pen(string color, int cost)
+        {
+            this.color = color;
+            this.cost = cost;
+        }
+    }
+    class Rectangle
+    {
+        public int a;
+        public int b;
+        public Rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Rectangle(int a)
+        {
+            this.a = a;
+            b = a;
+        }
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+        public int Square()
+        {
+            return a * b;
+        }
+    }
+
+}
